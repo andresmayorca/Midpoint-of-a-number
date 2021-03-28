03399d1b3ac7bd8b6784e0e4d24428b76ec1d7b5
Andres Mayorca <70079260+mayorcaandres@users.noreply.github.com>
2021-03-27 20:43:40 -0400

    Add files via upload

---
+++ Punto Intermedio.py
@@ -0,0 +1,10 @@
+# Sacar el punto intermedio de dos numeros 
+def intermedio(a, b):
+    punto = a + b 
+    division = punto // 2
+    print(division)
+
+a = int(input('Escriba n numero de su preferencia: '))
+b = int(input('Escriba n numero de su preferencia: '))
+intermedio(a, b)
+
